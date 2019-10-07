dfc46e8095deb46cf33bbe8886bafeed52c7a0a0
zengge2 <247019889@qq.com>
2019-10-07 20:10:19 +0800

    Initial commit

---
+++ AIDA64_KeyGen.h
@@ -0,0 +1,32 @@
+﻿
+// AIDA64_KeyGen.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CAIDA64KeyGenApp:
+// 有关此类的实现，请参阅 AIDA64_KeyGen.cpp
+//
+
+class CAIDA64KeyGenApp : public CWinApp
+{
+public:
+	CAIDA64KeyGenApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CAIDA64KeyGenApp theApp;
---
+++ AIDA64_KeyGenDlg.h
@@ -0,0 +1,36 @@
+﻿
+// AIDA64_KeyGenDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CAIDA64KeyGenDlg 对话框
+class CAIDA64KeyGenDlg : public CDialogEx
+{
+// 构造
+public:
+	CAIDA64KeyGenDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_AIDA64_KEYGEN_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
